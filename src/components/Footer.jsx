06d55eb50e899b9bad4da1a5b4fce830ae279bb6
ali-sshff <sharipoff.dev@gmail.com>
2023-09-05 18:46:57 +0200

    final changes have been made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
-
-// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt="logo" width='200px' height='40px' />
+        <Typography variant='h5' pb='40px' mt='20px'>
+          Made with Biceps By Ali
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -3,15 +3,19 @@ import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 
-import { exerciseOptions, fetchData } from '../utils/fetchData'
+
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData'
 
 import Detail from '../components/Detail'
 import ExerciseVideos from '../components/ExerciseVideos'
-import SimilarExercises from '../components/SearchExercises'
+import SimilarVideos from '../components/SimilarVideos'
 
 const ExerciseDetail = () => {
 
-  const [exerciseDetail, sestExerciseDetail] = useState({})
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseVideos, setExerciseVideos] = useState({})
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([])
+  const [equipmentExercises, setEquipmentExercises] = useState([])
   const { id } = useParams()
 
   useEffect(() => {
@@ -20,8 +24,17 @@ const ExerciseDetail = () => {
       const youtubeSearchUrl = `https://youtube-search-and-download.p.rapidapi.com`
 
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
-      sestExerciseDetail(exerciseDetailData)
-    }
+      setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideosData.contents)
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions)
+      setTargetMuscleExercises(targetMuscleExercisesData)
+
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions)
+      setEquipmentExercises(equipmentExercisesData)
+     }
 
     fetchExercisesData()
   }, [id])
@@ -29,8 +42,8 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail } />
-      <ExerciseVideos />
-      <SimilarExercises />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarVideos targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   )
 }
