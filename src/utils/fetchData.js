287e7be8c92e1424f7f9a37c0eb102da869c8800
ali-sshff <sharipoff.dev@gmail.com>
2023-09-05 15:54:23 +0200

    some changes have been made

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,8 +4,7 @@ export const exerciseOptions = {
     'X-RapidAPI-Key': import.meta.env.VITE_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
-};
-
+}; 
 export const youtubeOptions = {
   method: 'GET',
   params: {
